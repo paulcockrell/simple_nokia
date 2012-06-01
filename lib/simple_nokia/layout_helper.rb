1ccdc84ee56b6f8c3819fa291656fe35497bd123
Paul Cockrell <paulcockrell@gmail.com>
2012-06-01 17:59:12 +0100

    * Addition of map markers and map interactivity (controls)

--- lib/simple_nokia/layout_helper.rb
+++ lib/simple_nokia/layout_helper.rb
@@ -4,34 +4,65 @@ module SimpleNokia
 
     def nokia_map(placeholder, object, &block)
       object.html_options.merge!({:id=>placeholder})
-      object.options[:map][:renderTo] = placeholder
+      object.options[:renderTo] = placeholder
       generate_map(placeholder,object).concat(content_tag("div","", object.html_options))
     end
 
     def generate_map(placeholder, object, &block)
-      graph =<<-EOJS
+      map =<<-EOJS
       <script type="text/javascript">
       jQuery(function() {
-            console.log("Generating map");
-            var container = document.getElementById(#{object.options[:map][:renderTo].inspect});
-            nokia.Settings.set("appId", #{object.options[:app_id].to_json});
-            nokia.Settings.set("authenticationToken", #{object.options[:authentication_token].to_json});
-            var map = new nokia.maps.map.Display(container,
-              {
-                center: [#{object.options[:map][:latitude]}, #{object.options[:map][:longitude]}],
-                zoomLevel: #{object.options[:map][:zoom] || 10}
-              }
-            );
-        });
-        </script>
+        console.log("Generating map");
+        var container = document.getElementById(#{object.options[:renderTo].inspect});
+        nokia.Settings.set("appId", #{object.options[:app_id].to_json});
+        nokia.Settings.set("authenticationToken", #{object.options[:authentication_token].to_json});
+        var map = new nokia.maps.map.Display(container,
+          {
+            center: [#{object.options[:latitude]}, #{object.options[:longitude]}],
+            zoomLevel: #{object.options[:zoom] || 10}
+          }
+        );
+        // Interactivity options
+        //
+      EOJS
+      object.options[:components].each do |component|
+        map +=<<-EOJS
+          #{add_component(component)}
+        EOJS
+      end
+      map +=<<-EOJS
+        var _center = null;
+      EOJS
+      object.markers.each_with_index do |marker, idx|
+        map +=<<-EOJS
+        _center = new nokia.maps.geo.Coordinate(#{marker.latitude}, #{marker.longitude});
+        map.objects.add( new nokia.maps.map.StandardMarker(_center, {text:#{idx}}) );
+        EOJS
+      end
+      map +=<<-EOJS
+      });
+      </script>
       EOJS
 
       if defined?(raw)
-        return raw(graph)
+        return raw(map)
       else
-        return graph
+        return map
       end
 
+    end
+
+  private
+
+    def add_component(component)
+      js = []
+      component = component.to_s
+      valid_components = SimpleNokia::Map.valid_components
+      if valid_components.include?(component)
+          js << "map.addComponent( new nokia.maps.map.component.#{component}() );"
+      end
+
+      return js.join(" ")
     end
 
   end # class
--- lib/simple_nokia/map.rb
+++ lib/simple_nokia/map.rb
@@ -1,34 +1,56 @@
 module SimpleNokia
   class Map
     CANVAS_DEFAULT_HTML_OPTIONS = { :container => 'map', :style => "height: 300px, width:300px" }
-    attr_accessor :options, :html_options
+    VALID_COMPONENTS = %w{ Behavior ZoomBar Overview TypeSelector ScaleBar }
+    attr_accessor :options, :html_options, :markers
+
+    class Marker
+      attr_accessor :latitude, :longitude, :options
+
+      def initialize(latitude, longitude, opts={})
+        @latitude = latitude
+        @longitude = longitude
+        @options = opts
+      end
+    end
 
     def initialize(container = nil, html_opts = {})
+      @markers = []
       self.tap do |map|
         map.options ||= {}
         map.defaults_options
-        map.html_options = html_opts.merge(CANVAS_DEFAULT_HTML_OPTIONS)
+        map.html_options = html_opts.reverse_merge(CANVAS_DEFAULT_HTML_OPTIONS)
         map.html_options.merge({:container=>container}) if container
         yield map if block_given?
       end
     end
 
     def defaults_options
-      self.app_id = nil
-      self.authentication_token = nil
-      self.map({ :latitude => 50, :longitude => 0 })
+      self.options[:app_id] = nil
+      self.options[:authentication_token] = nil
+      self.options[:latitude] = 50
+      self.options[:longitude] = 0
+      self.options[:components] = ['ZoomBar', 'Behavior']
+    end
+
+    def self.valid_components
+      VALID_COMPONENTS
     end
 
     def method_missing(meth, opts = {})
       merge_options meth, opts
     end
 
+    def add_marker(lat, lon, opts = {})
+      raise if lat.nil? || lon.nil?
+      marker = Marker.new(lat, lon, opts)
+      @markers << marker
+    end
 
     private
 
     def merge_options(name, opts)
-      opts = @options.has_key?(name) ? @options[name].merge(opts) : opts
-      @options.merge!  name => opts
+      @options.merge! name => opts
     end
 
   end # class
